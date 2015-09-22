d215698edf2fdb7efd481ffd571d62c5b73b71a9
Alex Leventer <alexleventer@gmail.com>
2015-09-21 22:17:16 -0700

    maybe this will work

--- config/secrets.js
+++ config/secrets.js
@@ -1,12 +1,11 @@
 module.exports = {
   team: {
-    apiToken: process.env.APITOKEN,
     username: process.env.USERNAME,
     slackToken: process.env.SLACKTOKEN,
     slackTeam: process.env.SLACKTEAM
   },
   users: {
     //SlackUsername: ApiToken
-    al: process.env.APITOKEN
+    al: 'yourAPIToken'
   }
 }
