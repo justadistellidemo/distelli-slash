bf37a61e747a64f2647ea88dcd203fa57cb09d49
Alex Leventer <alexleventer@gmail.com>
2015-09-24 19:03:29 -0700

    fix missing contents

--- app.js
+++ app.js
@@ -26,8 +26,9 @@ function processQuery(slacktoken, slackteam_id, query, username, callback){
     //List apps
     if(query == "list apps"){
       request('https://api.distelli.com/' + secrets.team.username + '/apps?apiToken='
-        + secrets.users[username], function (error, response, body) {
+        + secrets.users[username] + '&max_results=50', function (error, response, body) {
           if(!error && response.statusCode == 200) {
+            var contents = JSON.parse(body);
             console.log(contents);
             var returnData = [];
             for(var i =0; i < contents.apps.length; i++){
